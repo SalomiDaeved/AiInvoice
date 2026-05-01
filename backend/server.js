3f80a06ae459d2dd9bc69471e4b7bbbe21013b21
SalomiDaeved <salomidaeved@gmail.com>
2026-05-01 09:56:27 +0530

    Folder Structure Defined

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';  
+import cors from 'cors';
+import 'dotenv/config';
+import{ clerkMiddleware } from '@clerk/express';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//MIDDLEWARES
+app.use(cors());
+app.use(express.json({limit: "20mb"}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+
+
+//DB
+
+
+
+//ROUTES
+
+
+app.get('/', (req, res) => {
+    res.send('API working');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port  http://localhost:${port}`);
+});
