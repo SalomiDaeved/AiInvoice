bec73fb206215f1b886755924de3469aea1825e3
SalomiDaeved <salomidaeved@gmail.com>
2026-05-01 10:17:46 +0530

    db.js added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import{ clerkMiddleware } from '@clerk/express';
+import {connectDB} from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(express.urlencoded({limit: "20mb", extended: true}));
 
 
 //DB
+connectDB();
 
 
 
